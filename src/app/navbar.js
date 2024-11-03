cdd8e1b5db47b75853ceb265bb512146323e6b3c
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-11-03 18:01:16 +0500

    first commit

---
+++ src/app/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <div>
+                Image
+            </div>
+            <div></div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
